d5ef8ea8ef9929d607cd29b6266d2e7a6d918b61
rjaldridge <rjaldridge@gmail.com>
2022-05-19 11:11:44 +0100

    Add files via upload

---
+++ gpx-reverser.pl
@@ -0,0 +1,72 @@
+#!perl
+
+if ($#ARGV) {
+	print <<ED;
+Usage       : perl $0 <route-file.gpx>
+Description : Takes a GPS GPX file and writes out a reverse route.
+Example     : perl $0 "Abinger Common.gpx"
+Author      : Richard Aldridge
+Created     : 10:34 19/05/2022
+Modified    : 10:34 19/05/2022
+ED
+
+	exit(1);
+}
+
+$fn_in = $fn_out = $ARGV[0];
+
+$fn_out =~ s/\.gpx/-reversed.gpx/;
+
+#print("fn_in = $fn_in\n");
+#print("fn_out = $fn_out\n");
+
+@gpx_points = ();
+
+open(INFILE,"<$fn_in");
+open(OUTFILE,">$fn_out");
+
+$in_point =0;
+
+$point = "";
+
+while($line=<INFILE>) {
+	chomp($line);
+
+	if(!$in_point) {
+	
+		if($line =~ /<rtept/) {
+			$in_point = 1;
+			
+			$point = "$line\n";
+		} elsif ($line =~ /<\/rte>/) {
+			#print("<!-- Reversed GPS GPX Points -->\n");
+			
+			while($rv_point = pop(@gpx_points)) {
+				print OUTFILE "$rv_point";
+			}
+			
+			print OUTFILE "$line\n";
+		} else {
+			print OUTFILE "$line\n";
+		}
+	} else {
+		if ($line =~ /<\/rtept/) {
+			$point .= "$line\n";
+			
+			push(@gpx_points,$point);
+			
+			$in_point = 0;
+		} else {
+			$point .= "$line\n";
+
+		}
+	}
+}
+
+close(OUTFILE);
+close(INFILE);
+
+exit(0);
+
+
+
